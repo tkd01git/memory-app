3734b99f3aee7b2e98ad2013143534f111a7ad20
tkd01git <yuhei.takadaa@gmail.com>
2026-04-19 02:21:26 +0900

    Update auth.js

--- api/drive/auth.js
+++ api/drive/auth.js
@@ -8,7 +8,8 @@ export default async function handler(req, res) {
       prompt: 'consent',
       scope: ['https://www.googleapis.com/auth/drive.file']
     });
-    return json(res, 200, { authUrl });
+    // フロント (app.js) は data.url を参照するため、キー名を "url" にそろえる
+    return json(res, 200, { url: authUrl });
   } catch (error) {
     return json(res, 500, { error: error.message });
   }
